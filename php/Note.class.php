315ba8623c53790db9103683dfbd6c67c0a7752b
Tom Selvi <tomselvi@berkeley.edu>
2013-02-01 23:35:06 -0800

    added template for Notes class

---
+++ php/Note.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class Notes
+{
+	static function generateTree ($options = array())
+	{
+		if ($options == array())
+		{
+			// Generate generic tree	
+		}
+		else
+		{
+			// Fields that CAN be specified (it can break if some are given but others are not...):
+			// college
+			// major
+			// field
+			// class
+			// subject
+			// difficulty
+			// tags (this will be done via PHP)
+		}
+	}
+}
+
+?>
